e3ed5690dde5094b8b099cfe2cb2ebc0efe9a20d
danilXX2000 <shalagin.danil@mail.ru>
2021-03-31 00:14:20 +0300

    mutation crossover

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+import random
 W = 512
 class Picture:
     def __init__(self,pict_size,elem_size):
@@ -13,8 +14,7 @@ class Picture:
 
         # array of color of element
         self.color_array=np.random.randint(0,256,(self.num_of_colomn,self.num_of_row,3),dtype=np.int32)
-
-        self.__fill_image_with_hexagons()
+        self.fill_image_with_hexagons()
 
     def fit_function(self,picture):
         if(picture.shape==self.picture.shape):
@@ -22,13 +22,42 @@ class Picture:
             fit = int(np.sum(a=result*result)**0.5)
         else:
             fit = 0
-        print(fit)
+        #print(fit)
         return fit
+    def mutation(self):
+        number_of_modification = int(self.num_of_row*self.num_of_colomn*0.10)
+        for i in range(number_of_modification):
+            x = random.randint(0,self.num_of_colomn-1)
+            y = random.randint(0, self.num_of_row-1)
+            self.color_array[x][y]=np.random.randint(0,256,(3))
+        self.fill_image_with_hexagons()
+
+    def crossover(Pict, otherPict):
+        chrom_size = int(Pict.num_of_row * Pict.num_of_colomn)
+        p1 = random.randint(0, chrom_size - 1)
+        p2 = random.randint(0, chrom_size - 1)
+        if p1 > p2:
+            temp_p = p1
+            p1 = p2
+            p2 = temp_p
+        for i in range(p1, p2):
+            temp = np.array([Pict.color_array.reshape((chrom_size, 3))[i][0],
+                             Pict.color_array.reshape((chrom_size, 3))[i][1],
+                             Pict.color_array.reshape((chrom_size, 3))[i][2]], dtype=np.int32)
+            Pict.color_array.reshape((chrom_size, 3))[i][0] = otherPict.color_array.reshape((chrom_size, 3))[i][0]
+            Pict.color_array.reshape((chrom_size, 3))[i][1] = otherPict.color_array.reshape((chrom_size, 3))[i][1]
+            Pict.color_array.reshape((chrom_size, 3))[i][2] = otherPict.color_array.reshape((chrom_size, 3))[i][2]
+
+            otherPict.color_array.reshape((chrom_size, 3))[i][0] = temp[0]
+            otherPict.color_array.reshape((chrom_size, 3))[i][1] = temp[1]
+            otherPict.color_array.reshape((chrom_size, 3))[i][2] = temp[2]
+        Pict.fill_image_with_hexagons()
+        otherPict.fill_image_with_hexagons()
 
     def show(self):
         cv.imwrite('output1.jpg', self.picture)
 
-    def __fill_image_with_hexagons(self):
+    def fill_image_with_hexagons(self):
         for i in range(self.num_of_colomn):
             y0 = self.elem_size * i / 2
             if (i + 1) % 2 == 1:
@@ -47,12 +76,42 @@ class Picture:
                 self.picture= cv.fillConvexPoly(self.picture, ppt1, (int(c1), int(c2), int(c3)), cv.LINE_4)
                 x0 = 6 * self.elem_size / 4 + self.elem_size / 5 + x0
 
+def crossover(Pict,otherPict):
+    chrom_size = int(Pict.num_of_row * Pict.num_of_colomn)
+    p1 = random.randint(0, chrom_size - 1)
+    p2 = random.randint(0, chrom_size - 1)
+    if p1>p2:
+        temp_p=p1
+        p1 = p2
+        p2=temp_p
+    for i in range(p1,p2):
+        temp = np.array([Pict.color_array.reshape((chrom_size,3))[i][0],
+                         Pict.color_array.reshape((chrom_size,3))[i][1],
+                         Pict.color_array.reshape((chrom_size,3))[i][2]],dtype=np.int32)
+        Pict.color_array.reshape((chrom_size, 3))[i][0] = otherPict.color_array.reshape((chrom_size, 3))[i][0]
+        Pict.color_array.reshape((chrom_size, 3))[i][1] = otherPict.color_array.reshape((chrom_size, 3))[i][1]
+        Pict.color_array.reshape((chrom_size, 3))[i][2] = otherPict.color_array.reshape((chrom_size, 3))[i][2]
+
+        otherPict.color_array.reshape((chrom_size, 3))[i][0] = temp[0]
+        otherPict.color_array.reshape((chrom_size, 3))[i][1] = temp[1]
+        otherPict.color_array.reshape((chrom_size, 3))[i][2] = temp[2]
+    Pict.fill_image_with_hexagons()
+    otherPict.fill_image_with_hexagons()
+
 # Create black empty images
 size = W, W, 3
 img = cv.imread('input.jpg')
 cv.imwrite('output2.jpg',img)
-chromosome = Picture(W,15)
-chromosome.fit_function(img)
+chromosome1 = Picture(W,15)
+chromosome1.fit_function(img)
+chromosome2 = Picture(W,15)
+chromosome2.fit_function(img)
+cv.imwrite('output1.jpg',chromosome1.picture)
+cv.imwrite('output2.jpg',chromosome2.picture)
+input()
+chromosome1.crossover(chromosome2)
+cv.imwrite('output1.jpg',chromosome1.picture)
+cv.imwrite('output2.jpg',chromosome2.picture)
 #atom_image = np.zeros(size, dtype=np.uint8)
 #hexagon(atom_image)
 
